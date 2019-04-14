cece8bc808332282d5d5d458ceaa5236bff5b027
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-14 10:03:36 +0200

    Create ToDo class

---
+++ src/main/java/com/romanstetsiuk/spring_tdd/model/ToDo.java
@@ -0,0 +1,56 @@
+package com.romanstetsiuk.spring_tdd.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class ToDo {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String title;
+    private String description;
+
+    public ToDo() {
+    }
+
+    public ToDo(String title, String description) {
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    @Override
+    public String toString() {
+        return "ToDo{" +
+                "id=" + id +
+                ", title='" + title + '\'' +
+                ", description='" + description + '\'' +
+                '}';
+    }
+}
