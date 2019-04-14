28622aa63b664b8fa2f9cae86968491b34277dbc
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-14 14:49:31 +0200

    return user date in UserDetailsServiceImpl

--- src/main/java/com/romanstetsiuk/spring_tdd/service/UserDetailsServiceImpl.java
+++ src/main/java/com/romanstetsiuk/spring_tdd/service/UserDetailsServiceImpl.java
@@ -1,15 +1,30 @@
 package com.romanstetsiuk.spring_tdd.service;
 
+import com.romanstetsiuk.spring_tdd.model.User;
+import com.romanstetsiuk.spring_tdd.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+import javax.persistence.EntityNotFoundException;
+
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
 
+    @Autowired
+    UserRepository userRepository;
+
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+        User user = userRepository
+                .findByUsername(s)
+                .orElseThrow(EntityNotFoundException::new);
+
+        UserDetails userDetails =
+                new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), null);
+
+        return userDetails;
     }
 }
