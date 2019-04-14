cca815056cabdb36a759efe3b960d3f458647135
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-14 14:31:31 +0200

    add HttpSecurity

--- src/main/java/com/romanstetsiuk/spring_tdd/security/WebSecurityConfig.java
+++ src/main/java/com/romanstetsiuk/spring_tdd/security/WebSecurityConfig.java
@@ -35,7 +35,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
+        http
+                .csrf().disable()
+                .authorizeRequests()
                 .antMatchers("/prywatna")
                 .authenticated()
                 .anyRequest()
