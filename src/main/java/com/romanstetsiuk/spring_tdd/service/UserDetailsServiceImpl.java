ac798a6ec5f501902edb3af5f0ccac7428792c9c
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-14 15:13:50 +0200

    add role to user

--- src/main/java/com/romanstetsiuk/spring_tdd/service/UserDetailsServiceImpl.java
+++ src/main/java/com/romanstetsiuk/spring_tdd/service/UserDetailsServiceImpl.java
@@ -3,12 +3,15 @@ package com.romanstetsiuk.spring_tdd.service;
 import com.romanstetsiuk.spring_tdd.model.User;
 import com.romanstetsiuk.spring_tdd.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import javax.persistence.EntityNotFoundException;
+import java.util.Arrays;
 
 @Service
 public class UserDetailsServiceImpl implements UserDetailsService {
@@ -22,8 +25,11 @@ public class UserDetailsServiceImpl implements UserDetailsService {
                 .findByUsername(s)
                 .orElseThrow(EntityNotFoundException::new);
 
+        GrantedAuthority admin = new SimpleGrantedAuthority("ADMIN");
+
         UserDetails userDetails =
-                new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(), null);
+                new org.springframework.security.core.userdetails.User(user.getUsername(),
+                        user.getPassword(), Arrays.asList(admin));
 
         return userDetails;
     }
