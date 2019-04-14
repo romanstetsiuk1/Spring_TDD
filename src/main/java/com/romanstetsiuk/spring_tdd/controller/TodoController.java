783f656780f67bb6e50945d2e5117070e4b34d02
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-14 10:16:51 +0200

    Write TodoController to findAll list

--- src/main/java/com/romanstetsiuk/spring_tdd/controller/TodoController.java
+++ src/main/java/com/romanstetsiuk/spring_tdd/controller/TodoController.java
@@ -1,13 +1,20 @@
 package com.romanstetsiuk.spring_tdd.controller;
 
+import com.romanstetsiuk.spring_tdd.service.TodoService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class TodoController {
 
-    @GetMapping("/test")
-    String test() {
+    @Autowired
+    TodoService todoService;
+
+    @GetMapping("/todosView")
+    String getTodosView(Model model) {
+        model.addAttribute("list: ", todoService.findAll());
         return "todos";
     }
 
