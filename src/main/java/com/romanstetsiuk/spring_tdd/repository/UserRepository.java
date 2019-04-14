0db69ddb0e3fd623d645fb03b71d563a229096ad
Roman Stetsiuk <romansteciuk021@gmail.com>
2019-04-14 14:38:24 +0200

    add classes User, UserRepository, UserDetailsService

---
+++ src/main/java/com/romanstetsiuk/spring_tdd/repository/UserRepository.java
@@ -0,0 +1,9 @@
+package com.romanstetsiuk.spring_tdd.repository;
+
+import com.romanstetsiuk.spring_tdd.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
